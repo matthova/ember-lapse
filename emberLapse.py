1bb9a0838f5ee2c507e729065bf9bd0d90832739
matthova <matthova87@gmail.com>
2014-12-17 19:49:40 +0000

    more wip

--- emberLapse.py
+++ emberLapse.py
@@ -6,12 +6,14 @@ import math
 
 layer_count = 0
 layers_per_photo = 4
-fps = 30
+total_layers = 0
+fps = 60
 seconds = 20
 photoTaken = False
+photoPin = 7
 
 def checkStatus(url, params):
-	global layer_count, layers_per_photo, photoTaken
+	global layer_count, layers_per_photo, photoTaken, total_layers
         r = requests.post(url, params)
 	decoded = json.loads(r.text.lower())
 	try:
@@ -20,35 +22,35 @@ def checkStatus(url, params):
 		if state == 'exposing':
 			if photoTaken == False:
 				photoTaken = True
-				print layer_count
-				print layers_per_photo
-				print layer_count % layers_per_photo
-				print ' '
 				if layer_count % layers_per_photo == 0:
-					GPIO.output(7,1)
-					time.sleep(.1)
-					GPIO.output(7,0)
+					takePhoto()
 				layer_count += 1
 		elif state == 'separating':
 			photoTaken = False
 		elif state == 'homing' or state == "movingtostartposition":
-			GPIO.output(7,1)
-			time.sleep(.1)
-			GPIO.output(7,0)
+			takePhoto()
+		elif state == 'home'
+			total_layers = 0
+
 	except(ValueError, KeyError, TypeError):
 		print "JSON format error"
 
+def takePhoto()
+	GPIO.output(photoPin, 1)
+	time.sleep(.1)
+	GPIO.output(photoPin, 0)
+
 def updateLayersPerPhoto(url, params):
-	global layers_per_photo
+	global layers_per_photo, total_layers
 	r = requests.post(url, params)
 	decoded = json.loads(r.text.lower())
 	try:
-		totalLayers = decoded['response']['state']
-		print totalLayers
-		if totalLayers > 0:
-			layers_per_photo = math.ceil(totalLayers / fps / seconds)
+		total_layers = int(decoded['response']['total_layers'])
+		print str(total_layers)
+		if total_layers > 0:
+			layers_per_photo = math.ceil(total_layers / fps / seconds)
 			print "Layers per photo: " + str(layers_per_photo)
-			print "Total photos to be taken: " + str(math.floor(totalLayers/layers_per_photo))
+			print "Total photos to be taken: " + str(math.floor(total_layers/layers_per_photo))
 	except(ValueError, KeyError, TypeError):
 		print "JSON format error"
 
@@ -62,9 +64,10 @@ try:
 
 	url = 'http://10.140.68.74/command'
 	params = {'command':'getstatus'}
-	updateLayersPerPhoto(url, params)
 	while(1):
 		checkStatus(url, params)
+		if total_layers == 0:
+			updateLayersPerPhoto(url, params)
 		time.sleep(.5)
 
 except KeyboardInterrupt:
